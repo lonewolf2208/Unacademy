3dc96c05620569c241fec4f4a2b2859952d252b7
Kunal Mehrotra <kunalmehrotra2001@gmail.com>
2022-01-03 18:11:49 +0530

    Added image picker from gallery in teachers profile page

--- app/src/main/java/com/example/unacademy/Ui/TeachersSide/HomePageTeachersSide.kt
+++ app/src/main/java/com/example/unacademy/Ui/TeachersSide/HomePageTeachersSide.kt
@@ -7,15 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.unacademy.R
+import com.example.unacademy.Repository.OtpRepo
+import com.example.unacademy.databinding.FragmentHomePageTeachersSideBinding
+import com.example.unacademy.databinding.FragmentOtpChangePasswordBinding
 
 class HomePageTeachersSide : Fragment() {
 
+    private var _binding :FragmentHomePageTeachersSideBinding?=null
+    private val binding
+        get() =_binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page_teachers_side, container, false)
+        _binding= FragmentHomePageTeachersSideBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/unacademy/Ui/TeachersSide/ProfileTeachersSide.kt
+++ app/src/main/java/com/example/unacademy/Ui/TeachersSide/ProfileTeachersSide.kt
@@ -1,23 +1,58 @@
 package com.example.unacademy.Ui.TeachersSide
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.unacademy.R
+import com.example.unacademy.databinding.FragmentProfileTeachersSideBinding
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+
+
+
 
 
 class ProfileTeachersSide : Fragment() {
 
+    private var binding:FragmentProfileTeachersSideBinding?=null
 
+    private var IMAGE_REQUEST_CODE=100
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_teachers_side, container, false)
+        binding= FragmentProfileTeachersSideBinding.inflate(inflater,container,false)
+        binding!!.setProfileImageTeachers.setOnClickListener {
+            pickImageGallery()
+        }
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!").addOnCompleteListener{
+            Toast.makeText(context,"Saved",Toast.LENGTH_LONG).show()
+        }
+        return binding!!.root
     }
 
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type="image/*"
+        startActivityForResult(intent,IMAGE_REQUEST_CODE)
+    }
 
+    override
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==IMAGE_REQUEST_CODE && resultCode==RESULT_OK)
+        {
+            binding?.setProfileImageTeachers?.setImageURI(data?.data)
+        }
+    }
 }
